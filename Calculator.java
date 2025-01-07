68fbde3cbaed28e31faf2b41dc17e720785e6bae
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-07 15:34:06 +0530

    multiply added

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class Calculator{
 
+
+
+   public static int multiply(int a, int b){
+
+    return a*b;
+   }
+
     public static void main(String []args){
 
         Scanner sc = new Scanner(System.in);
